3880b7397104c3f8f8750911d5113ec58370ca35
Amber Cyr <seylovie@gmail.com>
2019-09-30 20:51:37 -0700

    added a ListItem component

--- src/screens/ListScreen.tsx
+++ src/screens/ListScreen.tsx
@@ -1,55 +1,6 @@
 import React from 'react';
 import { View, Text, StyleSheet, FlatList } from 'react-native';
-
-type Friend = {
-	name: string;
-	age: number;
-};
-
-const ListScreen: React.SFC = () => {
-	const friends: Array<Friend> = [
-		{
-			name: 'Alex',
-			age: 24
-		},
-		{
-			name: 'Frank',
-			age: 26
-		},
-		{
-			name: 'Dawn',
-			age: 30
-		},
-		{
-			name: 'Ralph',
-			age: 28
-		},
-		{
-			name: 'Stacy',
-			age: 31
-		},
-		{
-			name: 'Megan',
-			age: 22
-		}
-	];
-	return (
-		<View style={styles.listStyle}>
-			<FlatList
-				showsVerticalScrollIndicator={false}
-				keyExtractor={friend => friend.name}
-				data={friends}
-				renderItem={({ item }) => (
-					<View style={styles.itemStyle}>
-						<Text style={styles.textStyle}>{`${item.name} - Age: ${
-							item.age
-						}`}</Text>
-					</View>
-				)}
-			/>
-		</View>
-	);
-};
+import { Badge, Icon, withBadge } from 'react-native-elements';
 
 const styles = StyleSheet.create({
 	listStyle: {
@@ -67,5 +18,58 @@ const styles = StyleSheet.create({
 		fontSize: 30
 	}
 });
+
+type Friend = {
+	name: string;
+	age?: number;
+};
+
+const friends: Array<Friend> = [
+	{
+		name: 'Alex',
+		age: 24
+	},
+	{
+		name: 'Frank',
+		age: 26
+	},
+	{
+		name: 'Dawn',
+		age: 30
+	},
+	{
+		name: 'Ralph',
+		age: 28
+	},
+	{
+		name: 'Stacy',
+		age: 31
+	},
+	{
+		name: 'Megan',
+		age: 22
+	}
+];
+
+function ListScreen() {
+	return (
+		<View style={styles.listStyle}>
+			<FlatList
+				showsVerticalScrollIndicator={false}
+				keyExtractor={friend => friend.name}
+				data={friends}
+				renderItem={({ item }) => <ListItem name={item.name} />}
+			/>
+		</View>
+	);
+}
+
+function ListItem(name: Friend) {
+	return (
+		<View style={styles.itemStyle}>
+			<Text style={styles.textStyle}>{name}</Text>
+		</View>
+	);
+}
 
 export default ListScreen;
