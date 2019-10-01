70db74fbba2ee8e3ae3ac6dd5ea5b0c874964ee6
Amber Cyr <seylovie@gmail.com>
2019-09-30 20:37:15 -0700

    refactored the component

--- src/screens/HomeScreen.tsx
+++ src/screens/HomeScreen.tsx
@@ -1,21 +1,40 @@
-import React from 'react';
-import { View, Text, StyleSheet } from 'react-native';
-
-const HomeScreen: React.SFC = () => {
-	return (
-		<View style={styles.container}>
-			<Text>Home Screen</Text>
-		</View>
-	);
-};
+import React, { FunctionComponent as FC, ReactNode } from 'react';
+import { View, StyleSheet } from 'react-native';
+import Box from '../components/Box';
 
 const styles = StyleSheet.create({
 	container: {
 		flex: 1,
-		backgroundColor: '#fff',
+		backgroundColor: 'black'
+	},
+	repeat: {
+		flex: 1,
+		flexDirection: 'row',
+		flexWrap: 'wrap',
 		alignItems: 'center',
 		justifyContent: 'center'
 	}
 });
 
-export default HomeScreen;
+interface Props {
+	numTimes: number;
+	styleContainer?: {};
+}
+
+export function HomeScreen() {
+	return (
+		<View style={styles.container}>
+			<Repeat numTimes={50} styleContainer={styles.repeat}>
+				<Box />
+			</Repeat>
+		</View>
+	);
+}
+
+const Repeat: FC<Props> = ({ numTimes, styleContainer, children }) => {
+	let items: ReactNode[] = [];
+	for (let i = 0; i < numTimes; i++) {
+		items.push(children);
+	}
+	return <View style={styleContainer}>{items}</View>;
+};
